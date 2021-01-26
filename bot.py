96c42f7e6298dcb213b587e25d0aa6d895a3c4a1
TooT95 <32481709+TooT95@users.noreply.github.com>
2021-01-26 19:52:01 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,53 +1,36 @@
 import os
-import requests
-import json
-from flask import Flask, render_template, request, jsonify
+
+from flask import Flask, request
+
 import telebot
-import datetime
 
-REGISTER_KEY = "Register"
-INFO_KEY = "Info"
-COR_DATA_KEY = "cor_data"
-NEXT_YEAR_KEY = "next_year"
-PREV_YEAR_KEY = "prev_year"
-COR_DATA_NAME = 'Корешок'
-BACK_TO_LIST_KEY = 'back_to_list'
-URL = "http://94.158.52.112:8080/UDN/hs/telegram/"
 TOKEN = '1643817918:AAHUPAqxhS6sMQY5MjpuEdY_4p-sqj5TMkQ'
-
-app = Flask(__name__)
 bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
-def hello():
-    if request.method == 'POST':
-        r = request.get_json()
-        chatid = r['message']['chat']['id']
-        message = r['message']['text']
-        hour = datetime.datetime.now().hour
-        if (message == '/start'):
-            if(hour<5 or hour>16):
-                bot.send_message(chatid,'Бот доступен с 10:00 по 22:00')
-            else:
-                startBot(chatid, r)
-        elif (message == '/help'):
-            bot.send_message(chatid, 'Help info')
-        else:
-            if(hour<5 or hour>16):
-                bot.send_message(chatid,'Бот доступен с 10:00 по 22:00')
-            else:
-                markup = telebot.types.InlineKeyboardMarkup(row_width=2)
-                markup = getMenu(markup)
-                bot.send_message(chatid, 'Выберите', reply_markup=markup)
-                # rr = requests.get(URL+''+str(request.data))
-        return jsonify(r)
-    else:
-        r = request.get_json()
-        bot.send_message('500324557', 'text:\n' + str(r))
-        return '<a href=\'login/\'>/login/</a><br> <a href=\'auth/\'>/auth/</a><br><a href=\'users/\'>/users/</a><br>'
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
-if __name__ == '__main__':
-    # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://your_heroku_project.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
